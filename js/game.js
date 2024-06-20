a5a940b13ff5c83164cf1b39076f9e6cf0314af7
Rabia Ürkmez <111rabiaurkmez@gmail.com>
2024-06-20 20:12:18 +0200

    update

--- js/game.js
+++ js/game.js
@@ -1,7 +1,34 @@
 let canvas;
 let world;
 let keyboard = new Keyboard();
+sound = false;
+music = new Audio('audio/bgAudio.mp3');
+music.volume = 0.1;
 
+
+/**
+ * Pauses the Backgroundmusic.
+ */
+
+function initBody() {
+    music.pause(); 
+}
+
+/**
+ * Switches the background music & sound.
+ */
+
+function mute() {
+    if (!sound) {
+        this.music.pause();
+        this.sound = true;
+        document.getElementById('change-speaker').style.backgroundImage = "url('img/pause.png')";
+    } else if (sound) {
+        this.music.play();
+        this.sound = false;
+        document.getElementById('change-speaker').style.backgroundImage = "url('img/play.png')";
+    }
+}
 
 
 /**
@@ -14,7 +41,7 @@ function initGame() {
     startGame();
     initLevel();
     world = new World(canvas, keyboard);
-
+    this.music.play();
 }
 
 
